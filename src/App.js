c344e578ad50c84cc08eb6a53d8b20d1b8fe0220
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-08-07 16:25:55 +0700

    Loop the Concept Component

--- src/App.js
+++ src/App.js
@@ -30,21 +30,14 @@ function App() {
     <div>
       <Header />
       <ul id="concepts">
-        <Concept
-          image={concepts[0].image}
-          title={concepts[0].title}
-          description={concepts[0].description}
-        />
-        <Concept
-          image={concepts[1].image}
-          title={concepts[1].title}
-          description={concepts[1].description}
-        />
-        <Concept
-          image={concepts[2].image}
-          title={concepts[2].title}
-          description={concepts[2].description}
-        />
+        {concepts.map((concept, index) => (
+          <Concept
+            key={index}
+            image={concept.image}
+            title={concept.title}
+            description={concept.description}
+          />
+        ))}
       </ul>
     </div>
   );
